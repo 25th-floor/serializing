1370c17e541f5e21bb583eacdd578ccaae670e18
Thomas Subera <ts@25th-floor.com>
2014-08-25 09:22:26 +0200

    fix date problem with javascript
    datetime serialization is incompatible with javascript date which needs a string representation as RFC2822 or ISO 8601
    @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
    
    therefore we override the date output of the default datetime serialization with RFC2822 output.

--- src/Serializer.php
+++ src/Serializer.php
@@ -7,139 +7,169 @@ namespace TwentyFifth\Serializing;
  */
 class Serializer
 {
-	/**
-	 * @param mixed $data
-	 * @param int   $steps
-	 *
-	 * @return array|null
-	 */
-	public static function serialize($data, $steps)
-	{
-		if ($steps < 0)
-			return self::serializeBasic($data, $steps);
+    /**
+     * @param mixed $data
+     * @param int   $steps
+     *
+     * @return array|null
+     */
+    public static function serialize($data, $steps)
+    {
+        if ($steps < 0) {
+            return self::serializeBasic($data, $steps);
+        }
 
-		if ($data instanceof Serializable) {
-			return self::serializeSerializable($data, $steps);
-		} else if (is_array($data)) {
-			return self::serializeArray($data, $steps);
-		} else if ($data instanceof \Iterator) {
-			return self::serializeIterator($data, $steps);
-		} else if ($data instanceof \IteratorAggregate) {
-			return self::serializeIterator($data->getIterator(), $steps);
-		} else {
-			return self::serializeBasic($data, $steps);
-		}
-	}
+        if ($data instanceof Serializable) {
+            return self::serializeSerializable($data, $steps);
+        } else {
+            if (is_array($data)) {
+                return self::serializeArray($data, $steps);
+            } else {
+                if ($data instanceof \Iterator) {
+                    return self::serializeIterator($data, $steps);
+                } else {
+                    if ($data instanceof \IteratorAggregate) {
+                        return self::serializeIterator($data->getIterator(), $steps);
+                    } else {
+                        return self::serializeBasic($data, $steps);
+                    }
+                }
+            }
+        }
+    }
 
-	/**
-	 * Returns all properties with its callable for a Serializable
-	 *
-	 * @param Serializable $serializable
-	 * @return Callable[]
-	 * @throws SerializingException
-	 */
-	public static function getSerializeMethods(Serializable $serializable)
-	{
-		if ($serializable instanceof AnnotationSerializable) {
-			return self::getSerializeMethodsForAnnotationSerializeable($serializable);
-		} elseif ($serializable instanceof MethodSerializable) {
-			return self::getSerializeMethodsForMethodSerializeable($serializable);
-		} else {
-			throw new SerializingException(__CLASS__.' is not able to serialize '.get_class($serializable));
-		}
-	}
+    /**
+     * Returns all properties with its callable for a Serializable
+     *
+     * @param Serializable $serializable
+     *
+     * @return Callable[]
+     * @throws SerializingException
+     */
+    public static function getSerializeMethods(Serializable $serializable)
+    {
+        if ($serializable instanceof AnnotationSerializable) {
+            return self::getSerializeMethodsForAnnotationSerializeable($serializable);
+        } elseif ($serializable instanceof MethodSerializable) {
+            return self::getSerializeMethodsForMethodSerializeable($serializable);
+        } else {
+            throw new SerializingException(__CLASS__ . ' is not able to serialize ' . get_class($serializable));
+        }
+    }
 
-	/**
-	 * @param Serializable $object
-	 * @param int          $steps
-	 * @return array
-	 */
-	protected static function serializeSerializable(Serializable $object, $steps)
-	{
-		$serialized = [];
+    /**
+     * @param Serializable $object
+     * @param int          $steps
+     *
+     * @return array
+     */
+    protected static function serializeSerializable(Serializable $object, $steps)
+    {
+        $serialized = [];
 
-		foreach (self::getSerializeMethods($object) as $property => $getter) {
-			$value = $getter();
-			$serialized[$property] = self::serialize($value, $steps - 1);
-		}
+        foreach (self::getSerializeMethods($object) as $property => $getter) {
+            $value = $getter();
+            $serialized[$property] = self::serialize($value, $steps - 1);
+        }
 
-		return $serialized;
-	}
+        return $serialized;
+    }
 
-	/**
-	 * @param AnnotationSerializable $serializable
-	 * @return array
-	 */
-	protected static function getSerializeMethodsForAnnotationSerializeable(AnnotationSerializable $serializable)
-	{
-		$methods = array();
+    private static $serializeMethodCache = array();
 
-		$properties = AnnotationParser::getProperties($serializable);
-		$ignores    = AnnotationParser::getIgnores($serializable);
+    /**
+     * @param AnnotationSerializable $serializable
+     *
+     * @return array
+     */
+    protected static function getSerializeMethodsForAnnotationSerializeable(AnnotationSerializable $serializable)
+    {
+//        $className = get_class($serializable);
+//        if (isset(self::$serializeMethodCache[$className])) {
+//            return self::$serializeMethodCache[$className];
+//        }
 
-		foreach ($properties as $property => $getter) {
-			if (in_array($getter, $ignores))
-				continue;
+        $methods = array();
 
-			$methods[$property] = array($serializable, $getter);
-		}
+        $properties = AnnotationParser::getProperties($serializable);
+        $ignores = AnnotationParser::getIgnores($serializable);
 
-		return $methods;
-	}
+        foreach ($properties as $property => $getter) {
+            if (in_array($getter, $ignores)) {
+                continue;
+            }
 
-	/**
-	 * @param MethodSerializable $serializable
-	 * @return array
-	 */
-	protected static function getSerializeMethodsForMethodSerializeable(MethodSerializable $serializable)
-	{
-		return $serializable->getSerializeMethods();
-	}
+            $methods[$property] = array($serializable, $getter);
+        }
 
-	/**
-	 * Serializes an array keeping its associative-ness
-	 *
-	 * @param array $array
-	 * @param int   $steps
-	 *
-	 * @return array
-	 */
-	protected static function serializeArray(array $array, $steps)
-	{
-		$serialized = array_map(function($element) use ($steps) { return self::serialize($element, $steps); }, $array);
+//        self::$serializeMethodCache[$className] = $methods;
 
-		$associative = array_sum(array_map('is_string', array_keys($array))) > 0;
+        return $methods;
+    }
 
-		return $associative ? $serialized : array_values($serialized);
-	}
+    /**
+     * @param MethodSerializable $serializable
+     *
+     * @return array
+     */
+    protected static function getSerializeMethodsForMethodSerializeable(MethodSerializable $serializable)
+    {
+        return $serializable->getSerializeMethods();
+    }
 
-	/**
-	 * Serializes an iterator to an array
-	 *
-	 * @param \Iterator $data
-	 * @param $steps
-	 * @return array
-	 */
-	protected static function serializeIterator(\Iterator $data, $steps)
-	{
-		return self::serializeArray(iterator_to_array($data, true), $steps);
-	}
+    /**
+     * Serializes an array keeping its associative-ness
+     *
+     * @param array $array
+     * @param int   $steps
+     *
+     * @return array
+     */
+    protected static function serializeArray(array $array, $steps)
+    {
+        $serialized = array_map(
+            function ($element) use ($steps) {
+                return self::serialize($element, $steps);
+            },
+            $array
+        );
 
-	/**
-	 * @param mixed $data
-	 * @param int   $steps
-	 *
-	 * @return mixed
-	 */
-	protected static function serializeBasic($data, $steps)
-	{
-		if ($data instanceof \DateTime) {
-			return $data;
-		}
+        $associative = array_sum(array_map('is_string', array_keys($array))) > 0;
 
-		if (is_object($data) || is_array($data))
-			return null;
+        return $associative ? $serialized : array_values($serialized);
+    }
 
-		return $data;
-	}
+    /**
+     * Serializes an iterator to an array
+     *
+     * @param \Iterator $data
+     * @param           $steps
+     *
+     * @return array
+     */
+    protected static function serializeIterator(\Iterator $data, $steps)
+    {
+        return self::serializeArray(iterator_to_array($data, true), $steps);
+    }
+
+    /**
+     * @param mixed $data
+     * @param int   $steps
+     *
+     * @return mixed
+     */
+    protected static function serializeBasic($data, $steps)
+    {
+        if ($data instanceof \DateTime) {
+            $arr = (array) $data;
+            $arr['date'] = $data->format(\DateTime::RFC2822);
+            return $arr;
+        }
+
+        if (is_object($data) || is_array($data)) {
+            return null;
+        }
+
+        return $data;
+    }
 }
